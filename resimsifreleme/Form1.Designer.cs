3788f37db2f4dee3c7f2c283e13229b1d0d3e7f2
sema.nur <sema.nur@SEMANUR-PC>
2024-06-09 16:37:22 +0300

    Add project files.

---
+++ resimsifreleme/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace resimsifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnSaveEncryptedImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(935, 737);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(1159, 56);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(75, 35);
+            this.btnLoadImage.TabIndex = 1;
+            this.btnLoadImage.Text = "upload";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(1159, 127);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 38);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "şifrele";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(1139, 199);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(95, 31);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "şifreyi çöz";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btnSaveEncryptedImage
+            // 
+            this.btnSaveEncryptedImage.Location = new System.Drawing.Point(1139, 265);
+            this.btnSaveEncryptedImage.Name = "btnSaveEncryptedImage";
+            this.btnSaveEncryptedImage.Size = new System.Drawing.Size(95, 31);
+            this.btnSaveEncryptedImage.TabIndex = 4;
+            this.btnSaveEncryptedImage.Text = "kaydet";
+            this.btnSaveEncryptedImage.UseVisualStyleBackColor = true;
+            this.btnSaveEncryptedImage.Click += new System.EventHandler(this.btnSaveEncryptedImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1303, 589);
+            this.Controls.Add(this.btnSaveEncryptedImage);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnSaveEncryptedImage;
+    }
+}
+
---
+++ resimsifreleme/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace resimsifreleme
+{
+        public partial class Form1 : Form
+        {
+            private Bitmap originalImage;
+            private Bitmap encryptedImage;
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void btnLoadImage_Click(object sender, EventArgs e)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = new Bitmap(openFileDialog.FileName);
+                    pictureBox1.Image = originalImage;
+                }
+            }
+
+            private void btnEncrypt_Click(object sender, EventArgs e)
+            {
+                if (originalImage != null)
+                {
+                    encryptedImage = EncryptImage(originalImage);
+                    pictureBox1.Image = encryptedImage;
+                }
+            }
+
+            private void btnDecrypt_Click(object sender, EventArgs e)
+            {
+                if (encryptedImage != null)
+                {
+                    Bitmap decryptedImage = EncryptImage(encryptedImage); // XOR işlemi tersine çevrilebilir.
+                    pictureBox1.Image = decryptedImage;
+                }
+            }
+
+            private Bitmap EncryptImage(Bitmap image)
+            {
+                Bitmap encryptedImage = new Bitmap(image.Width, image.Height);
+
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+                        Color encryptedColor = Color.FromArgb(pixelColor.R ^ 0xFF, pixelColor.G ^ 0xFF, pixelColor.B ^ 0xFF); // Basit XOR işlemi.
+                        encryptedImage.SetPixel(x, y, encryptedColor);
+                    }
+                }
+
+                return encryptedImage;
+            }
+
+            private void btnSaveEncryptedImage_Click(object sender, EventArgs e)
+            {
+                if (encryptedImage != null)
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Bitmap Image|*.bmp";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        encryptedImage.Save(saveFileDialog.FileName);
+                    }
+                }
+            }
+    }
+    }
+
